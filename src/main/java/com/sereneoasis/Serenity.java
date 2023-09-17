eb238ae41d4a73249dc312a89f5f3aaaac9acf1b
Sakrajin <asurasakrajin@gmail.com>
2023-09-17 22:46:53 +0100

    First commit

---
+++ src/main/java/com/sereneoasis/Serenity.java
@@ -0,0 +1,20 @@
+package com.sereneoasis;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Serenity extends JavaPlugin {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
---
+++ src/main/java/com/sereneoasis/listeners/SerenityListener.java
@@ -0,0 +1,15 @@
+package com.sereneoasis.listeners;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class SerenityListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e)
+    {
+
+    }
+}
